a527e646c26297ba806265e7ab818590e1765ab1
Okami Dash <okami@doubledash.org>
2022-04-13 13:51:03 +0100

    Initial init (dark forked from repo)

---
+++ packages/mds/mds.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Request struct {
+	Location string `json:"location"`
+}
+
+type Response struct {
+	StatusCode int               `json:"statusCode,omitempty"`
+	Headers    map[string]string `json:"headers,omitempty"`
+	Body       string            `json:"body,omitempty"`
+}
+
+func Main(in Request) (*Response, error) {
+	if in.Location == "" {
+		return nil, errors.New("location must be passed")
+	}
+
+	return &Response{
+		Body: fmt.Sprintf("Hello, from %s", in.Location),
+	}, nil
+}
